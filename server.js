1ddbc304c4f058cd37b5bc6ac67108190f3cab68
sid12261996 <sidharthrkc@gmail.com>
2019-09-29 21:08:27 +0530

    layout comes to play

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express'),
-  app = express(),
-  path = require('path'),
-  cors= require('cors');
-
-app.use(cors());
+    app = express(),
+    path = require('path');
 
 
-app.use(express.static(path.join(__dirname+'/dist/chetan-construction-company')));
-console.log(path.join(__dirname+'/dist/chetan-construction-company'));
-app.listen(process.env.PORT||5000,()=>console.log('Connected!'));
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(path.join(__dirname + '/dist/wrapItUp'));
+app.listen(process.env.PORT || 5000, () => console.log('Connected!'));
 
-app.get('/*',(req,res)=>{
-  console.log("route..!")
-
-
-  res.sendFile(path.join(__dirname,'/dist/chetan-construction-company/index.html'));
+app.get('/*', (req, res) => {
+    console.log("route..!");
+    res.sendFile(path.join(__dirname, '/public/home-hero-slideshow.html'));
 });
